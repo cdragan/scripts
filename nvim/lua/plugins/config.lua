10bb672fcc0e62881123f939d8e60000fd196196
Chris Dragan <chris@dragan.dev>
2024-02-09 23:19:20 +0000

    select color theme

--- nvim/lua/plugins/config.lua
+++ nvim/lua/plugins/config.lua
@@ -1,5 +1,11 @@
 return {
     {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin"
+        }
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         opts = {
             ensure_installed = {
