307b138bd5a0ed07019239090e9137f17a1dd367
Chris Dragan <chris@dragan.dev>
2024-02-25 17:42:47 +0000

    fix escape sequence in nvim keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', '\<C-s>', ':w<CR>', { desc = "Save current buffer", noremap = true, silent = true })
-vim.keymap.set('i', '\<C-s>', '<Esc>:w<CR>a', { desc = "Save current buffer", noremap = true, silent = true })
+vim.keymap.set('n', '\\<C-s>', ':w<CR>', { desc = "Save current buffer", noremap = true, silent = true })
+vim.keymap.set('i', '\\<C-s>', '<Esc>:w<CR>a', { desc = "Save current buffer", noremap = true, silent = true })
