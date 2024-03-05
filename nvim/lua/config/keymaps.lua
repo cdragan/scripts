2f2919c26078c260db61af0cab06539b7cbf7169
Chris Dragan <chris@dragan.dev>
2024-03-05 09:53:13 +0000

    add map for document symbols

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '\\<C-s>', ':w<CR>', { desc = "Save current buffer", noremap = true, silent = true })
 vim.keymap.set('i', '\\<C-s>', '<Esc>:w<CR>a', { desc = "Save current buffer", noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ds', require("telescope.builtin").lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
