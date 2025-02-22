11d56a3979e09440fce7a4d33f32c310588b2d65
Chris Dragan <chris@dragan.dev>
2025-02-22 17:50:12 +0000

    update Kos syntax

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,3 +6,4 @@ vim.opt.virtualedit = "all"
 vim.opt.clipboard = "unnamedplus"
 vim.g.autoformat = false
 vim.g.root_spec = { { ".clangd", ".p4config", ".git", "lua" }, "cwd" }
+vim.filetype.add({ extension = { kos = "kos" } })
